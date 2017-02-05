f39a9085d646d945edac35ef3dd0cec183867964
nomche111020 <nomche_bezhanoski@outlook.com>
2017-02-05 18:42:10 +0100

    New project

---
+++ Proekt_Studentski_Studentski_Domovi/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Proekt_Studentski_Studentski_Domovi.Startup))]
+namespace Proekt_Studentski_Studentski_Domovi
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
